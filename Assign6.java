f1111881abc72f18b456cbf6425f96d66af00624
Sankar27 <90670458+Sankar27@users.noreply.github.com>
2021-09-28 07:36:36 +0530

    Question 6

---
+++ Assign6.java
@@ -0,0 +1,15 @@
+class Assign6 
+{
+	public static void main(String[] args) 
+	{
+		  int [] arr = {11,22,55,33,22,77,55,99,44,77,33};   
+          
+        System.out.println("Duplicate elements : " );  
+        for(int i = 0; i < arr.length; i++) {  
+            for(int j = i + 1; j < arr.length; j++) {  
+                if(arr[i] == arr[j])  
+                    System.out.println(arr[j]);  
+			}
+		}
+	}
+}
